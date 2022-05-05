1f73459bc6afb880305e2be49136acf6659cef67
AlexMancinas <alexman381@gmail.com>
2022-05-05 10:59:18 -0700

    MultiHilos_ready

--- PracticaMultiHilos/PracticaMultiHilos/Form1.Designer.cs
+++ PracticaMultiHilos/PracticaMultiHilos/Form1.Designer.cs
@@ -28,13 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.btnSalair = new System.Windows.Forms.Button();
+            this.btnSiguiente = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(266, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(501, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(266, 58);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(501, 23);
+            this.progressBar2.TabIndex = 1;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(266, 107);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(501, 23);
+            this.progressBar3.TabIndex = 2;
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.Location = new System.Drawing.Point(12, 12);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(207, 118);
+            this.btnInicio.TabIndex = 3;
+            this.btnInicio.Text = "Inicio";
+            this.btnInicio.UseVisualStyleBackColor = true;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // btnSalair
+            // 
+            this.btnSalair.Location = new System.Drawing.Point(46, 314);
+            this.btnSalair.Name = "btnSalair";
+            this.btnSalair.Size = new System.Drawing.Size(173, 95);
+            this.btnSalair.TabIndex = 4;
+            this.btnSalair.Text = "Salir";
+            this.btnSalair.UseVisualStyleBackColor = true;
+            this.btnSalair.Click += new System.EventHandler(this.btnSalair_Click);
+            // 
+            // btnSiguiente
+            // 
+            this.btnSiguiente.Location = new System.Drawing.Point(598, 314);
+            this.btnSiguiente.Name = "btnSiguiente";
+            this.btnSiguiente.Size = new System.Drawing.Size(169, 95);
+            this.btnSiguiente.TabIndex = 5;
+            this.btnSiguiente.Text = "Siguiente";
+            this.btnSiguiente.UseVisualStyleBackColor = true;
+            this.btnSiguiente.Click += new System.EventHandler(this.btnSiguiente_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSiguiente);
+            this.Controls.Add(this.btnSalair);
+            this.Controls.Add(this.btnInicio);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.Button btnSalair;
+        private System.Windows.Forms.Button btnSiguiente;
     }
 }
 
--- PracticaMultiHilos/PracticaMultiHilos/Form1.cs
+++ PracticaMultiHilos/PracticaMultiHilos/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace PracticaMultiHilos
 {
@@ -15,6 +16,73 @@ namespace PracticaMultiHilos
         public Form1()
         {
             InitializeComponent();
+        }
+
+        Thread hilo;
+        delegate void delegado(int valor);
+
+        private void btnSalair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            progressBar2.Value = 0;
+            progressBar3.Value = 0;
+        }
+
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            hilo = new Thread(new ThreadStart(Proceso1));
+            hilo.Start();
+            hilo = new Thread(new ThreadStart(Proceso2));
+            hilo.Start();
+            hilo = new Thread(new ThreadStart(Proceso3));
+            hilo.Start();
+        }
+
+        public void Proceso1()
+        {
+
+            for (int i = 0; i <= 100; i++)
+            {
+                delegado md = new delegado(actualizar1);
+                this.Invoke(md, new object[] { i });
+                Thread.Sleep(70);
+            }
+        }
+        public void actualizar1(int valor)
+        {
+            progressBar1.Value = valor;
+        }
+        public void Proceso2()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                delegado md = new delegado(actualizar2);
+                this.Invoke(md, new object[] { i });
+                Thread.Sleep(100);
+            }
+        }
+        public void actualizar2(int valor)
+        {
+            progressBar2.Value = valor;
+        }
+
+        public void Proceso3()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                delegado md = new delegado(actualizar3);
+                this.Invoke(md, new object[] { i });
+                Thread.Sleep(50);
+            }
+        }
+        public void actualizar3(int valor)
+        {
+            progressBar3.Value = valor;
         }
     }
 }
